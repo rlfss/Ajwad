b6ceaeb85686eefc3cfb759f3b534b6a3c3f276d
Ghaith Gammar <info@saharaintergroup.com>
2019-02-19 11:36:58 +0200

    Add files via upload

---
+++ purchase_access_rule/__manifest__.py
@@ -0,0 +1,14 @@
+{
+	'name': "Purchase Access Rule",
+	'summary': "Access rule for purchase order and RFQ",
+	'auther': "Muram Makkawi",
+	'category':'Purchase',
+	'depends': ['purchase'],
+	'data': [
+		'security/ir.model.access.csv',
+		'security/purchase_security.xml'
+		],		
+	'installable': True,
+	'license': 'OEEL-1',
+
+}
