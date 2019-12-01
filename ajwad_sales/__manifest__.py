f38d0fb9171b69f2f53b64f03285b357f6bc48be
med-sahara <55658971+med-sahara@users.noreply.github.com>
2019-12-02 00:41:16 +0200

    Update __manifest__.py

--- ajwad_sales/__manifest__.py
+++ ajwad_sales/__manifest__.py
@@ -4,7 +4,7 @@
         'version': '12.0.1.0.0',
         'category': 'Sale',
         'author': '',
-        'depends': ['base', 'sale', 'sale_order_type','sales_team'],
+        'depends': ['base', 'sale','sales_team'],
         'data': [
                 'security/sale_groups.xml',
                 'views/sale_order.xml'
