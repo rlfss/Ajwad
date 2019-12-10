fe483689b31dcd38e0d72f59d2aa3b478e74a778
med-sahara <55658971+med-sahara@users.noreply.github.com>
2019-12-10 14:40:23 +0200

    Workshop New

--- workshop/models/workshop.py
+++ workshop/models/workshop.py
@@ -3,6 +3,7 @@
 from dateutil.relativedelta import relativedelta
 from odoo import api, fields, models, SUPERUSER_ID, _
 from odoo.addons import decimal_precision as dp
+from datetime import datetime, timedelta
 
 class WorkShop(models.Model):
     _name = 'workshop'
@@ -42,6 +43,49 @@ class WorkShopVehicles(models.Model):
 
     name = fields.Char('Name', readonly=True , default=lambda x: str('New'))
 
+    make = fields.Many2one('workshop.vehicles.make', string='Make')
+    model = fields.Many2one('workshop.vehicles.model', string='Model')
+    color = fields.Many2one('workshop.vehicles.color', string='Color')
+
+    year = fields.Integer(string='Year') 
+
+    fuel_type = fields.Selection([
+        ('petrol', 'Petrol'),
+        ('diesel', 'Diesel'),
+        ('gas', 'Gas'),
+        ('electric', 'Electric'),
+    ], string="Fuel Type")
+
+    odometer = fields.Integer(string='Last Odometer') 
+
+    liscence_plate = fields.Char('Liscence Plate')
+    last_owner = fields.Many2one('res.partner', string='Last Owner')
+    vin = fields.Char('VIN', size=17)
+
+    _sql_constraints = [
+        ('vin_uniq', 'unique (vin)', 'The VIN Must be unique !'),
+    ]
+
+
+
+class WorkShopVehiclesMake(models.Model):
+    _name = 'workshop.vehicles.make'
+    _description = 'Vehicles Make'
+
+    name = fields.Char('Make')
+
+
+class WorkShopVehiclesModel(models.Model):
+    _name = 'workshop.vehicles.model'
+    _description = 'Vehicles Model'
+
+    name = fields.Char('Model')
+
+class WorkShopVehiclesColor(models.Model):
+    _name = 'workshop.vehicles.color'
+    _description = 'Vehicles Color'
+
+    color = fields.Char('Color')
 
 
 class WorkShopInvoice(models.Model):
