5995f24c1d4038639d1108eb2c496d2017641e82
med-sahara <55658971+med-sahara@users.noreply.github.com>
2019-12-14 13:43:53 +0200

    workshop new update

--- workshop/models/repair.py
+++ workshop/models/repair.py
@@ -38,12 +38,13 @@ class WorkShopTechnicians(models.Model):
 
 class WorkShopWorkCenters(models.Model):
     _name = 'workshop.work.centers'
+    _inherit = ['mail.thread']
     _description = 'Work Centers'
 
     name = fields.Char('Name')
     code = fields.Char('Code')
 
-    technicians = fields.Many2one('workshop.technicians', string='Technicians List', required=True)
+    technicians = fields.Many2many('workshop.technicians', string='Technicians List', required=True)
     partner_id = fields.Many2one('res.partner', related='technicians.related_partner', string='Related Partner')
     
     analytic_account = fields.Many2one('account.analytic.account', string='Analytic Account')
