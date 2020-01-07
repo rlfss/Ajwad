130d5afed698ecfaa446df8fa71801b786aa2a7b
med-sahara <55658971+med-sahara@users.noreply.github.com>
2020-01-07 14:52:47 +0200

    Update sale_order_discount.py

--- sale_order_discount/models/sale_order_discount.py
+++ sale_order_discount/models/sale_order_discount.py
@@ -10,5 +10,38 @@ class PurchaseCustomOrderLine(models.Model):
 
     global_discount = fields.Float(string='Global Discount', digits=dp.get_precision('Discount'), default=0.0)
     global_discount_type = fields.Selection([('fixed', 'Fixed'),('percent ', 'Percent ')], string="Discount Type")
+    total_global_discount  = fields.Float(string='Global Discount Total', digits=dp.get_precision('Discount'), default=0.0)
+    total_undiscount = fields.Float(string='UnDiscounted Total', digits=dp.get_precision('Discount'), default=0.0)
 
-    
+    def _total_undiscount(self):
+        for order in self:
+            for line in order:
+                order.total_undiscount += line.price_unit
+            
+
+    @api.onchange('global_discount_type')
+    def _discount_type(self):
+        for order in self:
+            order.global_discount = 0
+            
+
+    @api.onchange('global_discount')
+    def _discount_amount_total(self):
+        for order in self:
+            total_undiscount = 0.0
+            for line in order:
+                total_undiscount += line.price_unit
+            discount_limit = order.team_id.discount_limit
+            discount_limit_total = total_undiscount  * discount_limit / 100
+            
+            if order.global_discount_type == 'fixed':
+                if total_undiscount - order.amount_total < order.global_discount:
+                    order.total_global_discount = order.global_discount
+                else:
+                    raise ValidationError('Discount Value Greater Than Discount Limit' + ' ' + str(discount_limit_total))                
+            if order.global_discount_type == 'percent':
+                discount = sum(order.order_line.mapped('price_subtotal')) * order.global_discount / 100
+                if total_undiscount - order.amount_total < discount:
+                    order.total_global_discount = discount
+                else:
+                    raise ValidationError('Discount Value Greater Than Discount Limit' + ' ' + str(discount_limit_total))                
