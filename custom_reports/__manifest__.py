1157a51e4dff1803bc3dfe1cafbed2d06d438c18
med-sahara <55658971+med-sahara@users.noreply.github.com>
2020-01-08 21:48:49 +0200

    custom_reports

---
+++ custom_reports/__manifest__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Customize Reports',
+    'version': '12.0.0.2',
+    'depends': ['base', 'account', 'sale'],
+    'data': [
+		"invoice_report/report_account.xml",
+        # "sale_report/sale_report.xml",
+             ],
+    'installable': True,
+    'auto_install': False,
+}
