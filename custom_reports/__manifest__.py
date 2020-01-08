0613a8eb66444698d0cc584e695ff6cffa93145d
med-sahara <55658971+med-sahara@users.noreply.github.com>
2020-01-08 22:06:26 +0200

    Update __manifest__.py

--- custom_reports/__manifest__.py
+++ custom_reports/__manifest__.py
@@ -5,7 +5,7 @@
     'depends': ['base', 'account', 'sale'],
     'data': [
 		"invoice_report/report_account.xml",
-        # "sale_report/sale_report.xml",
+		 "sale_report/sale_report.xml",
              ],
     'installable': True,
     'auto_install': False,
